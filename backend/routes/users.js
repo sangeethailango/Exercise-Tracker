7c8c9e8f0e76df95f44f28a1e36e62be1862c13d
sangeetha <sangeethailango21@gmail.com>
2023-12-05 16:09:58 +0530

    get and post request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,25 @@
-const router = require('express').Router();
+// Creates new router object 
+const router = require('express').Router();                      
+
 let User = require('../models/user.model');
 
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.get( '/',  (req, res) => {
+  User.find()                                                 // User.find() is a mongoose function which will return all the User data
+    .then(users => res.json(users))                           // If the query runs successfully, the response will be given in json format
+    .catch(err => res.status(400).json('Error: ' + err));     // If it is an error, then 400 error will come.
 });
 
-router.route('/add').post((req, res) => {
-  const username = req.body.username;
+router.post( '/add' ,(req, res) => {
+  const username = req.body.username;                         // Will get the username from the request body. 
 
-  const newUser = new User({username});
+  console.log(username)                                      
 
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+  const newUser = new User({username});                       //create a inserted username in the User model.  
+
+  newUser.save()                                              // A MongoDB function will save the newly added user to the database  
+    .then(() => res.json('User added!'))                      // If the query runs successfully, a 'User added!' json message
+    .catch(err => res.status(400).json('Error: ' + err));     // If it is an error, 400 error will come. 
 });
+
 
 module.exports = router;
