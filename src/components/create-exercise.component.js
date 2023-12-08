528b0d5fca84f6215fbea398cdcf817306f9ebac
sangeetha <sangeethailango21@gmail.com>
2023-12-08 22:08:27 +0530

    Connecting backend to frontend

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios';
 import "react-datepicker/dist/react-datepicker.css";
 
 
@@ -23,11 +24,20 @@ export default class CreateExercise extends Component {
     }
   }
   // Will automatically called right before anything loads on the page.
+
   componentDidMount() {
-    this.setState({ 
-      users: ['test user'],
-      username: 'test user'
-    });
+    axios.get('http://localhost:5000/users/')
+    .then(response => {
+      if (response.data.length > 0) {
+        this.setState({   
+          users: response.data.map(user => user.username),
+          username: response.data[0].username
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })    
   }
   // event handler functions
   onChangeUsername(e) {
@@ -62,62 +72,63 @@ export default class CreateExercise extends Component {
       date: this.state.date,
     };
   console.log(exercise);
+
+  axios.post('http://localhost:5000/exercises/add', exercise).then(res => console.log(res.data));
+
   window.location = '/';
   }
 
   render(){
     return(
       <div>
-        <h3>Create New Exercise Log</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group"> 
-            <label>Username: </label>
+        <h3 className="max-w-sm mx-auto py-5" >Create New Exercise Log</h3>
+        <form className="max-w-sm mx-auto" onSubmit={this.onSubmit}>
+          <div className="mb-5">
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username:</label>
             <select 
                 required
-                className="form-control"
                 value={this.state.username}
-                onChange={this.onChangeUsername}>
+                onChange={this.onChangeUsername}
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 {
                   this.state.users.map(function(user) {
                     return <option 
                       key={user}
                       value={user}>{user}
                       </option>;
-                  })
-                }
+                })}
             </select>
-          </div> 
-          <div className="form-group"> 
-            <label>Description: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.description}
-                onChange={this.onChangeDescription}
-                />
-          </div>
-          <div className="form-group">
-            <label>Duration (in minutes): </label>
-            <input 
-                type="text" 
-                className="form-control"
-                value={this.state.duration}
-                onChange={this.onChangeDuration}
-                />
-          </div>
-          <div className="form-group">
-            <label>Date: </label>
-            <div>
-              <DatePicker
-                selected={this.state.date}
-                onChange={this.onChangeDate}
+            </div>
+            <div className="mb-5">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description: </label>   
+              <input  
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+              required/>
+            </div>
+            <div className="mb-5">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Duration (in minutes): </label>
+              <input 
+              type="text" 
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"    
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
               />
             </div>
-          </div>
-
-          <div className="form-group">
-            <button type="submit" value="Create Exercise Log" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">submit</button>
-          </div>
+            <div className="mb-5">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date: </label>
+              <div>
+                <DatePicker
+                  selected={this.state.date}
+                  onChange={this.onChangeDate}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <button type="submit" value="Create Exercise Log" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">submit</button>
+            </div>
         </form>
       </div>
     )
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class ExercisesList extends Component {        
+const Exercise = props => (
+  <tr>
+    <td scope="col" className="px-6 py-3">{props.exercise.username}</td>
+    <td scope="col" className="px-6 py-3">{props.exercise.description}</td>
+    <td scope="col" className="px-6 py-3">{props.exercise.duration}</td>
+    <td scope="col" className="px-6 py-3">{props.exercise.date.substring(0,10)}</td>
+  </tr>
+)
+
+export default class ExercisesList extends Component {  
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {exercises: []};
+  }  
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/exercises/')
+     .then(response => {
+       this.setState({ exercises: response.data });
+     })
+     .catch((error) => {
+        console.log(error);
+     })
+  }  
+
+  exercisesList() {
+    return this.state.exercises.map(currentexercise => {
+      return <Exercise exercise={currentexercise} />;
+    })
+  }
+  
   render(){
     return(
       <div>
-        <p>This is ExercisesList</p>
+        <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            User name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Description
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Duration
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Date
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                   { this.exercisesList()} 
+                </tbody>
+            </table>
+        </div>
       </div>
     )
   }
